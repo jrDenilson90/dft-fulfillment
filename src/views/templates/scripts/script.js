36851c03dcd8b69131c23b0e9fc60168f35c1aae
Denilson Jose da Silva Junior <denilson.junior@029968-SP-MB.local>
2025-03-19 17:23:41 -0300

    add: iniciando projeto

---
+++ src/views/templates/scripts/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const elemento = document.querySelector('.parallax');
+    const parallaxImage = document.querySelector('.parallaxImage');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                // Inicia o efeito parallax
+                window.addEventListener('scroll', parallaxEffect);
+                observer.unobserve(entry.target); // Para parar de observar após a primeira visibilidade
+                console.log("AQUI");
+            }
+        });
+    });
+
+    observer.observe(elemento);
+
+    function parallaxEffect() {
+        const scrollPosition = window.scrollY;
+        parallaxImage.style.transform = 'translateY(' + scrollPosition * 0.4 + 'px)';
+    }
+});
