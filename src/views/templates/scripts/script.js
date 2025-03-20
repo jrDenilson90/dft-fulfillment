8b052ea4782fb3c0eeff107385a2f1f04f9f693a
Denilson Jose da Silva Junior <denilson.junior@029968-SP-MB.local>
2025-03-20 12:15:08 -0300

    update: finalizando html

--- src/views/templates/scripts/script.js
+++ src/views/templates/scripts/script.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const elemento = document.querySelector('.parallax');
-    const parallaxImage = document.querySelector('.parallaxImage');
+    // const elemento = document.querySelector('.parallax');
+    // const parallaxImage = document.querySelector('.parallaxImage');
 
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                // Inicia o efeito parallax
-                window.addEventListener('scroll', parallaxEffect);
-                observer.unobserve(entry.target); // Para parar de observar após a primeira visibilidade
-                console.log("AQUI");
-            }
-        });
-    });
+    // const observer = new IntersectionObserver((entries) => {
+    //     entries.forEach(entry => {
+    //         if (entry.isIntersecting) {
+    //             // Inicia o efeito parallax
+    //             window.addEventListener('scroll', parallaxEffect);
+    //             observer.unobserve(entry.target); // Para parar de observar após a primeira visibilidade
+    //             console.log("AQUI");
+    //         }
+    //     });
+    // });
 
-    observer.observe(elemento);
+    // observer.observe(elemento);
 
-    function parallaxEffect() {
-        const scrollPosition = window.scrollY;
-        parallaxImage.style.transform = 'translateY(' + scrollPosition * 0.4 + 'px)';
-    }
+    // function parallaxEffect() {
+    //     const scrollPosition = window.scrollY;
+    //     parallaxImage.style.transform = 'translateY(' + scrollPosition * 0.4 + 'px)';
+    // }
 });
