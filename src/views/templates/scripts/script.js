e6ed7555742fd38fc44bea49d7dd2ba05014e76d
Denilson Jose da Silva Junior <denilson.junior@029968-SP-MB.local>
2025-03-24 17:06:26 -0300

    update - atualizacao de conteudo e css

--- src/views/templates/scripts/script.js
+++ src/views/templates/scripts/script.js
@@ -1,22 +1,57 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // const elemento = document.querySelector('.parallax');
-    // const parallaxImage = document.querySelector('.parallaxImage');
+    // Seleciona o elemento de parallax e a imagem dentro dele
+    const parallaxImage = document.querySelector('.parallaxImage');
 
-    // const observer = new IntersectionObserver((entries) => {
-    //     entries.forEach(entry => {
-    //         if (entry.isIntersecting) {
-    //             // Inicia o efeito parallax
-    //             window.addEventListener('scroll', parallaxEffect);
-    //             observer.unobserve(entry.target); // Para parar de observar após a primeira visibilidade
-    //             console.log("AQUI");
-    //         }
-    //     });
-    // });
+    // Função para aplicar o efeito de parallax
+    function parallaxEffect() {
+        const scrollPosition = window.scrollY;
+        // parallaxImage.style.transform = 'translateY(' + scrollPosition * -0.02 + '%)';
+    }
 
-    // observer.observe(elemento);
+    // Adiciona o evento de scroll para aplicar o efeito de parallax
+    window.addEventListener('scroll', parallaxEffect);
 
-    // function parallaxEffect() {
-    //     const scrollPosition = window.scrollY;
-    //     parallaxImage.style.transform = 'translateY(' + scrollPosition * 0.4 + 'px)';
-    // }
+    // Seleciona todos os elementos que precisam ser observados
+    const elementosParaObservar = document.querySelectorAll('.faixa, .line, .box, .boxInfos');
+
+    // Define as opções do IntersectionObserver
+    const observerOptions = {
+        root: null, // Usar a viewport como contêiner
+        rootMargin: '0px',
+        threshold: 0.1 // Acionar quando 10% do elemento estiver visível
+    };
+
+    // Função para criar o IntersectionObserver
+    function createObserver() {
+        const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    // Verifica se a largura da janela é maior ou igual a 768px
+                    if (window.innerWidth >= 768) {
+                        entry.target.classList.add('anima');
+                    }
+                    observer.unobserve(entry.target); // Parar de observar uma vez que a classe é adicionada
+                }
+            });
+        }, observerOptions);
+
+        // Observa cada elemento selecionado
+        elementosParaObservar.forEach(elemento => {
+            observer.observe(elemento);
+        });
+    }
+
+    // Cria o observer inicialmente
+    createObserver();
+
+    // Atualiza o observer ao redimensionar a janela
+    window.addEventListener('resize', function() {
+        // Remove a classe 'anima' de todos os elementos
+        elementosParaObservar.forEach(elemento => {
+            elemento.classList.remove('anima');
+        });
+
+        // Recria o observer para aplicar as mudanças
+        createObserver();
+    });
 });
