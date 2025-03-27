1d3255e4b6241ee5eb9a4a0537dc6c429bdf1922
Denilson Jose da Silva Junior <denilson.junior@029968-SP-MB.local>
2025-03-27 14:05:00 -0300

    update: finalizando v1

--- src/views/templates/scripts/script.js
+++ src/views/templates/scripts/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Inicio do Menu
+    // Função para adicionar evento de scroll suave a botões
     function addScrollEvent(buttonSelector, targetSelector) {
         document.querySelector(buttonSelector)?.addEventListener('click', function() {
             document.querySelector(targetSelector).scrollIntoView({
@@ -8,80 +8,65 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
     }
-    
+
+    // Adiciona eventos de scroll suave para os botões do menu
     addScrollEvent('.btnPrograma', '.programa');
     addScrollEvent('.btnVantagens', '.vantagens');
     addScrollEvent('.btnIncremento', '.incremento');
     addScrollEvent('.btnBeneficios', '.beneficios');
     addScrollEvent('.btnDuvidas', '.duvidas');
     addScrollEvent('.btnCadastro', '.cadastro');
-    // Fim do Menu
+    addScrollEvent('.btnCadastroFixed', '.cadastro');
 
-    // Função para fixar a sec2
-    window.addEventListener('scroll', function() {
-        const element = document.querySelector('.sec2');
-        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        const elementOffsetTop = element.offsetTop;
-
-        if (scrollTop >= elementOffsetTop) {
-            element.classList.add('fixed');
+    // Evento de scroll para mostrar/ocultar botão fixo de cadastro
+    document.addEventListener('scroll', function() {
+        const sec2 = document.querySelector('.sec2');
+        const btnCadastroFixed = document.querySelector('.centerButton');
+        const secCadastro = document.querySelector('.sec9');
+    
+        if (sec2 && btnCadastroFixed && secCadastro) {
+            const rectSec2 = sec2.getBoundingClientRect();
+            const rectCadastro = secCadastro.getBoundingClientRect();
+    
+            if (rectSec2.bottom < 0 && rectCadastro.top > window.innerHeight) {
+                btnCadastroFixed.classList.add('block');
+            } else {
+                btnCadastroFixed.classList.remove('block');
+            }
         }
     });
 
-    // Usar IntersectionObserver para detectar quando sec1 aparece
-    const sec1 = document.querySelector('.sec1');
-    const sec2 = document.querySelector('.sec2');
-
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                sec2.classList.remove('fixed');
-            }
-        });
-    }, {
-        root: null, // Usar a viewport como contêiner
-        threshold: 0.1 // Acionar quando 10% do sec1 estiver visível
-    });
-
-    observer.observe(sec1);
-
-    // Seleciona o elemento de parallax e a imagem dentro dele
-    const parallaxImage = document.querySelector('.parallaxImage');
-
     // Função para aplicar o efeito de parallax
     function parallaxEffect() {
+        const parallaxImage = document.querySelector('.parallaxImage');
         const scrollPosition = window.scrollY;
-        // parallaxImage.style.transform = 'translateY(' + scrollPosition * -0.02 + '%)';
+        // Ajuste o fator de multiplicação para evitar que a imagem seja cortada
+        // parallaxImage.style.transform = 'translateY(' + scrollPosition * -0.1 + 'px)';
     }
 
     // Adiciona o evento de scroll para aplicar o efeito de parallax
     window.addEventListener('scroll', parallaxEffect);
 
-    // Seleciona todos os elementos que precisam ser observados
-    const elementosParaObservar = document.querySelectorAll('.faixa, .line, .box, .boxInfos');
-
-    // Define as opções do IntersectionObserver
+    // Configuração do IntersectionObserver para animações
+    const elementosParaObservar = document.querySelectorAll('.faixa, .line, .box, .boxInfos, .barra');
     const observerOptions = {
-        root: null, // Usar a viewport como contêiner
+        root: null,
         rootMargin: '0px',
-        threshold: 0.1 // Acionar quando 10% do elemento estiver visível
+        threshold: 0.1
     };
 
-    // Função para criar o IntersectionObserver
     function createObserver() {
         const observer = new IntersectionObserver((entries, observer) => {
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
-                    // Verifica se a largura da janela é maior ou igual a 768px
                     if (window.innerWidth >= 768) {
                         entry.target.classList.add('anima');
                     }
-                    observer.unobserve(entry.target); // Parar de observar uma vez que a classe é adicionada
+                    observer.unobserve(entry.target);
                 }
             });
         }, observerOptions);
 
-        // Observa cada elemento selecionado
         elementosParaObservar.forEach(elemento => {
             observer.observe(elemento);
         });
@@ -92,12 +77,43 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Atualiza o observer ao redimensionar a janela
     window.addEventListener('resize', function() {
-        // Remove a classe 'anima' de todos os elementos
         elementosParaObservar.forEach(elemento => {
             elemento.classList.remove('anima');
         });
-
-        // Recria o observer para aplicar as mudanças
         createObserver();
+    });
+
+    // Ajuste capa
+    const maskImgCapa = document.querySelector('.maskImg');
+    setTimeout(() => {
+        maskImgCapa.classList.add('invisible');
+    }, 1200);
+
+    // Adiciona tabindex aos elementos filhos da classe .dafitiStructure
+    const elements = document.querySelectorAll('.dafitiStructure h1, .dafitiStructure h2, .dafitiStructure h3, .dafitiStructure h4, .dafitiStructure h5, .dafitiStructure p, .dafitiStructure button, .dafitiStructure a, .dafitiStructure img[alt], .dafitiStructure video, .dafitiStructure .sr-only');
+    let currentIndex = 1;
+
+    elements.forEach((element) => {
+        // Verifica se o elemento já tem tabindex
+        if (element.hasAttribute('tabindex')) {
+            return;
+        }
+
+        if (element.tagName === 'IMG') {
+            if (element.getAttribute('alt').trim() !== '') {
+                element.setAttribute('tabindex', currentIndex);
+                currentIndex++;
+            }
+        } else if (element.tagName === 'BUTTON' || element.tagName === 'VIDEO') {
+            element.setAttribute('tabindex', currentIndex);
+            currentIndex++;
+        } else if (element.tagName === 'A') {
+            element.setAttribute('tabindex', currentIndex);
+            element.setAttribute('aria-label', 'Esse link enviará você para outra página.');
+            currentIndex++;
+        } else if (element.tagName !== 'P' || (element.parentElement.tagName !== 'BUTTON' && element.parentElement.tagName !== 'A')) {
+            element.setAttribute('tabindex', currentIndex);
+            currentIndex++;
+        }
     });
 });
